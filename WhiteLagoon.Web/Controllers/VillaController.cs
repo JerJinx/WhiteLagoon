afe8ca69cb9bbe7bb065baa5b2da5dde877eb993
DESKTOP-V8M24KM\Jinelin Briones <jerome.briones@corptech.it>
2025-08-15 00:33:10 +0800

    End of Section 3

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController(ApplicationDbContext context) : Controller
+    {
+        public IActionResult Index()
+        {
+            var villas = context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                context.Villas.Add(villa);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(villa);
+        }
+    }
+}
